165c92b29b1ae120bf4e5291aaa84c052eff5de4
ashiph-ui <76753545+ashiph-ui@users.noreply.github.com>
2023-07-21 15:08:03 +0100

    added the hands object and processing

--- HandTracking.py
+++ HandTracking.py
@@ -5,9 +5,15 @@ import time
 # First setting up the which camera to use - this is the video object:
 cap = cv2.VideoCapture(0) # 0 indicates which camera to use
 
+mpHands = mp.solutions.hands # Hands object - Formality
+hands = mpHands.Hands() # Hands object - frames
+
+
 while True:
     success, img = cap.read() # Frame
-    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+    results = hands.process(imgRGB) # Process the image
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
